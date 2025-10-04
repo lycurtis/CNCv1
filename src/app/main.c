2f1ddb7c5cd739d47ad72e1eb93cd2c39dd693a9
lycurtis <lycurtis046@gmail.com>
2025-10-04 11:09:05 -0700

    clean main

--- src/app/main.c
+++ src/app/main.c
@@ -8,18 +8,6 @@ int main(void){
     stepgen_dir(true); 
     stepgen_set_hz(1000); // 1 kHz step rate
     stepgen_start();
-
-    // // Enable GPIOA clock
-    // RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-
-    // // Set PA5 as general-purpose output
-    // GPIOA->MODER &= ~(3u << (5*2));
-    // GPIOA->MODER |=  (1u << (5*2));
-
-    while(1){
-        // GPIOA->BSRR |= (1<<5);  // Set the pin PA5
-        // delay(5000000);
-		// GPIOA->BSRR |= ((1<<5) <<16);  // Reset pin PA5
-        // delay(5000000);
-    }
+    
+    while(1){}
 }
