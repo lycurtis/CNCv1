7772bdf28e0a277368704a2cc63953bd07ae26dd
lycurtis <lycurtis046@gmail.com>
2025-10-10 23:33:24 -0700

    fix(pins): no overlap

--- src/bsp/bsp_pins.h
+++ src/bsp/bsp_pins.h
@@ -8,10 +8,10 @@
 #define X_STEP_AF_VAL 2UL // AF2 -> TIM3_CH1
 
 #define X_DIR_PORT  GPIOB
-#define X_DIR_PIN   0UL // PB0
+#define X_DIR_PIN   4UL // PB4
 
 #define X_EN_PORT GPIOB
-#define X_EN_PIN 1UL // PB1 (note: LOW = enable)
+#define X_EN_PIN 12UL // PB12 (note: LOW = enable)
 
 // Y-Axis
 #define Y_STEP_PORT GPIOA
@@ -19,10 +19,10 @@
 #define Y_STEP_AF_VAL 2UL // AF2 -> TIM3_CH2
 
 #define Y_DIR_PORT GPIOB
-#define Y_DIR_PIN 10UL // PB10
+#define Y_DIR_PIN 5UL // PB5
 
 #define Y_EN_PORT GPIOB
-#define Y_EN_PIN 11UL // PB11
+#define Y_EN_PIN 13UL // PB13
 
 // Z-Axis
 #define Z_STEP_PORT GPIOB
@@ -30,21 +30,21 @@
 #define Z_STEP_AF_VAL 2UL // AF2 -> TIM3_CH2
 
 #define Z_DIR_PORT GPIOC
-#define Z_DIR_PIN 13UL // PB13
+#define Z_DIR_PIN 2UL // PC2
 
 #define Z_EN_PORT GPIOB
-#define Z_EN_PIN 12UL // PB12
+#define Z_EN_PIN 14UL // PB14
 
 ////////// IO //////////
 // Limit switches / E-Stop
-#define X_MIN_PORT  GPIOC
-#define X_MIN_PIN   (1U<<13)
-#define ESTOP_PORT  GPIOB
-#define ESTOP_PIN   (1U<<2)
+// #define X_MIN_PORT  GPIOC
+// #define X_MIN_PIN   (1U<<13)
+// #define ESTOP_PORT  GPIOB
+// #define ESTOP_PIN   (1U<<2)
 
-// Debug UART
-#define DBG_UART USART2
-#define DBG_TX_PORT GPIOA
-#define DBG_TX_PIN  (1U<<2) // PA2 AF7
-#define DBG_RX_PORT GPIOA
-#define DBG_RX_PIN  (1U<<3) // PA3 AF7
+// // Debug UART
+// #define DBG_UART USART2
+// #define DBG_TX_PORT GPIOA
+// #define DBG_TX_PIN  (1U<<2) // PA2 AF7
+// #define DBG_RX_PORT GPIOA
+// #define DBG_RX_PIN  (1U<<3) // PA3 AF7
