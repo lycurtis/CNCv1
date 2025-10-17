ef34d0de35343a323526dbf0c7c0e1d207fb7476
lycurtis <lycurtis046@gmail.com>
2025-10-17 16:28:28 -0700

    refactor(CMakeLists.txt): create sub-directories

--- cmake/toolchain-arm-none-eabi.cmake
+++ cmake/toolchain-arm-none-eabi.cmake
@@ -3,21 +3,6 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY) # Avoid try-compile running a host linker
 
-# set(TOOLCHAIN_PREFIX arm-none-eabi)
-
-# find_program(CMAKE_C_COMPILER arm-none-eabi-gcc)
-# find_program(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
-
-# find_program(CMAKE_OBJCOPY  arm-none-eabi-objcopy)
-# find_program(CMAKE_SIZE     arm-none-eabi-size)
-
-# find_program(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}-gcc)
-# find_program(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
-# find_program(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
-
-# find_program(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}-objcopy)
-# find_program(CMAKE_SIZE    ${TOOLCHAIN_PREFIX}-size)
-
 # Point to your desired GNU Arm toolchain root
 set(TOOLCHAIN_DIR "C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2021.10")
 
@@ -30,3 +15,4 @@ set(CMAKE_ASM_COMPILER "${TOOLCHAIN_DIR}/bin/arm-none-eabi-gcc.exe")
 set(CMAKE_AR        "${TOOLCHAIN_DIR}/bin/arm-none-eabi-ar.exe")
 set(CMAKE_OBJCOPY   "${TOOLCHAIN_DIR}/bin/arm-none-eabi-objcopy.exe")
 set(CMAKE_SIZE      "${TOOLCHAIN_DIR}/bin/arm-none-eabi-size.exe")
+
