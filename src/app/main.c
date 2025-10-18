a18e5d7e80d24ed4ae9516bfcfd426245e55b2ef
lycurtis <lycurtis046@gmail.com>
2025-10-18 00:08:40 -0700

    chore(.vscode): trim configs; rely on CMake Tools for IntelliSense

--- src/app/main.c
+++ src/app/main.c
@@ -2,17 +2,17 @@
 #include "motion_units.h"
 #include "stepgen_pwm_tim3.h"
 
-
 #define DIR_CW true
 #define DIR_CCW false
 
 int main(void) {
     app_init();
 
-    // Example: +10 mm @ 1200 mm/min
     stepgen_enable(AXIS_X, true);
     stepgen_dir(AXIS_X, DIR_CW);
-    stepgen_move_n(AXIS_X, mm_to_steps(AXIS_X, 10.0f), feed_to_hz(AXIS_X, 1200.0f));
+    stepgen_move_n(AXIS_X,
+                   mm_to_steps(AXIS_X, 50.0f),
+                   feed_to_hz(AXIS_X, 2200.0f)); // Example: +50 mm @ 2200 mm/min
     while (stepgen_busy(AXIS_X)) {
     }
 
