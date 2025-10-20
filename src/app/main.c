4b1b33133b1bc62b71d83b73d46118f31d452443
lycurtis <lycurtis046@gmail.com>
2025-10-19 20:03:19 -0700

    refactor(config): add cfg subdirectory

--- src/app/main.c
+++ src/app/main.c
@@ -5,6 +5,12 @@
 #define DIR_CW true
 #define DIR_CCW false
 
+/**
+ * X-Axis: CW = negative direction; CCW = positive direction
+ * Y-Axis: CW = negative direction; CCW = positive direction
+ * Z-Axis: CW = positive drection; CCW = negative direction
+ */
+
 int main(void) {
     app_init();
 
@@ -12,15 +18,15 @@ int main(void) {
     // stepgen_enable(AXIS_Y, true);
     // stepgen_enable(AXIS_Z, true);
 
-    stepgen_dir(AXIS_X, DIR_CW);
-    // stepgen_dir(AXIS_Y, DIR_CCW);
+    stepgen_dir(AXIS_X, DIR_CCW);
+    // stepgen_dir(AXIS_Y, DIR_CW);
     // stepgen_dir(AXIS_Z, DIR_CW);
 
     // IMPORTANT: single shared step frequency (TIM3 ARR is shared; last set wins)
     uint32_t hz = feed_to_hz(AXIS_X, 1200.0f); // pick one base; all axes will use this hz
 
     // Arm all three axes back-to-back (no waits in between) → they run together
-    stepgen_move_n(AXIS_X, mm_to_steps(AXIS_X, 10.0f), hz);
+    stepgen_move_n(AXIS_X, mm_to_steps(AXIS_X, 20.0f), hz);
     while (stepgen_busy(AXIS_X)) {
     }
 
